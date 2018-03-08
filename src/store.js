c53e60483a1fd8637ae972eb8e9419f559820fe2
Pablo Alexandre <pabloalexandre.nave@gmail.com>
2018-03-08 19:38:22 -0300

    Adding localStorage tests and creating a getJSONData method to return a parsed object

--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@ import createStore from 'redux-zero';
 import { applyMiddleware } from 'redux-zero/middleware';
 import { connect } from 'redux-zero/devtools';
 
+import { getJSONData } from './utils'
+
 const INITIAL_STATE = {
   users: [],
   messages: [],
-  currentUser: JSON.parse(localStorage.getItem('currentUser') || '{}'),
+  currentUser: getJSONData('currentUser'),
 };
 
 const middlewares = connect ? applyMiddleware(connect(INITIAL_STATE)) : [];
--- src/utils/localStorage.js
+++ src/utils/localStorage.js
@@ -2,3 +2,7 @@ export function setJSONData(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
   return data;
 }
+
+export function getJSONData(key) {
+  return JSON.parse(localStorage.getItem(key) || '{}');
+}
