cb38ce6d6ce4723986a08aedb7d18194708ab309
Leonardo Villela <leonardo.villela@concrete.com.br>
2018-03-09 11:44:35 -0300

    Improving test to user services.

--- src/modules/user/services.spec.js
+++ src/modules/user/services.spec.js
@@ -8,7 +8,7 @@ describe('Test services unit', () => {
     jest.resetModules();
   });
 
-  it('Create user', async () => {
+  it('Create user retreving user last id', async () => {
     const userNameStub = chance.name();
     const userStub = { id: chance.natural() };
     const newUserStub = { id: chance.natural(), name: chance.name() };
@@ -31,6 +31,30 @@ describe('Test services unit', () => {
 
     expect(getMock).toBeCalledWith(lastUserStub);
     expect(postMock).toBeCalledWith(indexStub, { id: userStub.id + 1, name: userNameStub });
+    expect(setJSONDataMock).toBeCalledWith('currentUser', newUserStub);
+  });
+
+  it('Should generate newId without users', async () => {
+    const newUserStub = { id: 0, name: chance.name() };
+
+    const postMock = jest.fn(() => Promise.resolve({ data: newUserStub }));
+    const getMock = jest.fn(() => (({ data: [] })));
+
+    const indexStub = chance.url();
+    const lastUserStub = chance.url();
+
+    const setJSONDataMock = jest.fn();
+
+    jest.doMock('./apiRoutes', () => ({ index: indexStub, lastUser: lastUserStub }));
+    jest.doMock('../shared', () => ({ apiClient: { post: postMock, get: getMock } }));
+    jest.doMock('../../utils', () => ({ localStorage: { setJSONData: setJSONDataMock } }));
+
+    const services = require('./services').default;
+
+    await services.createUser(newUserStub.name);
+
+    expect(getMock).toBeCalledWith(lastUserStub);
+    expect(postMock).toBeCalledWith(indexStub, { id: newUserStub.id , name: newUserStub.name });
     expect(setJSONDataMock).toBeCalledWith('currentUser', newUserStub);
   });
 });
